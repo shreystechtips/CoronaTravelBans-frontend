af929277e23466135d9885d39444bf789e921d42
shrey <shreystechtips@gmail.com>
2020-03-25 18:45:55 -0700

    cache update

--- public/service-worker.js
+++ public/service-worker.js
@@ -18,7 +18,7 @@
 'use strict';
 
 // CODELAB: Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v1';
+const CACHE_NAME = 'static-cache-v2';
 
 const FILES_TO_CACHE = [
     '/index.html',
